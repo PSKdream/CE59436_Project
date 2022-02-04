a4a8d6962e29efc38bebb7be8df1052c36164ccb
View <thanathip804@gmail.com>
2022-02-04 12:22:51 +0700

    warpPerspective

--- project.py
+++ project.py
@@ -2,9 +2,16 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-
+pts1 = []
+pts2 = [[0, 0], [300, 0], [300, 300], [0, 300]]
+def onClick(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if len(pts1) < 4:
+            pts1.append([x, y])
+            print(pts1)
+cv2.namedWindow('frame')
+cv2.setMouseCallback('frame', onClick)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 while True:
     _, frame = cap.read()
     imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -12,19 +19,23 @@ while True:
     thresh = cv2.medianBlur(thresh, 5)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    if len(contours) != 0:
-        # draw in blue the contours that were founded
-        cv2.drawContours(frame, contours, -1, 255, 3)
-        # find the biggest countour (c) by the area
-        c = max(contours, key=cv2.contourArea)
+    if len(pts1) == 4:
+        T = cv2.getPerspectiveTransform(np.float32(pts1), np.float32(pts2))
+        frame = cv2.warpPerspective(imgray, T, (300, 300))
 
-        approx = cv2.approxPolyDP(c, 0.1 * cv2.arcLength(c, True), True)
-        cv2.drawContours(frame, [approx], 0, (0, 0, 255), 5)
+    # if len(contours) != 0:
+    #     # draw in blue the contours that were founded
+    #     cv2.drawContours(frame, contours, -1, 255, 3)
+    #     # find the biggest countour (c) by the area
+    #     c = max(contours, key=cv2.contourArea)
+    #
+    #     approx = cv2.approxPolyDP(c, 0.1 * cv2.arcLength(c, True), True)
+    #     cv2.drawContours(frame, [approx], 0, (0, 0, 255), 5)
+    #
+    #     if len(approx) == 4:
+    #         position = [i[0] for i in approx]  #position for perspective
 
-        if len(approx) == 4:
-            position = [i[0] for i in approx]  #position for perspective
 
 
-
-    cv2.imshow('t', frame)
+    cv2.imshow('frame', frame)
     cv2.waitKey(1)
